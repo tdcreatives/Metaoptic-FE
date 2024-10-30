5935b353453d88eb72d9e2f237a0efcccd06c956
kt <khoa.ta@skillspar.com>
2024-10-30 22:49:50 +0700

    header scale

--- src/app/layouts/header/index.js
+++ src/app/layouts/header/index.js
@@ -2,22 +2,26 @@ import Image from 'next/image';
 
 const Header = () => {
     return (
-        <div className='font-bold px-[4.5vw] py-[48px] flex justify-between max-w-[1920px] mx-auto'>
+        <div
+            className='font-bold flex justify-between max-w-[1920px] mx-auto'
+            style={{
+                padding: '4.44vh 4.5vw',
+            }}>
             <Image
                 src='/logo.svg'
                 alt='Logo'
                 width='0'
                 height='0'
-                className='w-[19.5vw] h-auto cursor-pointer'
+                className='w-[19.5vw] h-auto cursor-pointer' // 375px converted to 19.5vw
                 priority
             />
 
             <Image
                 src='/menu.svg'
-                alt='Logo'
+                alt='Menu'
                 width='0'
                 height='0'
-                className='w-[5vw] h-auto cursor-pointer'
+                className='w-[5vw] h-auto cursor-pointer' // 96px converted to 5vw
                 priority
             />
         </div>
