3cef738144c7e835798558ff7cb0c1a65a30b3d3
kt <khoa.ta@skillspar.com>
2024-10-31 17:18:29 +0700

    footer

--- src/app/layouts/footer/index.js
+++ src/app/layouts/footer/index.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const Footer = () => {
     return (
-        <div className='bg-[#131313] futura-book px-[5.2vw] xl:py-[12.31vh] py-[48px]'>
+        <div className='bg-[#131313] futura-book px-[5.2vw] xl:py-[12.31vh] py-[48px] relative'>
             <div className='flex justify-between'>
                 <div className='flex flex-col xl:gap-[5.6vh] gap-4'>
                     <p className='text-[#888888] tracking-wider xl:text-[3.77vw] text-[24px]'>
@@ -18,7 +18,7 @@ const Footer = () => {
                         </p>
 
                         <a href='mailto:sales@metaoptics.com.sg'>
-                            <p className='text-[#E0E1E0]'>
+                            <p className='text-[#E0E1E0] mt-2'>
                                 {' '}
                                 <span className='text-[#888888]'>Email: </span>
                                 sales@metaoptics.com.sg
@@ -32,7 +32,7 @@ const Footer = () => {
                     width='0'
                     height='0'
                     priority
-                    className='h-auto'
+                    className='h-auto xl:block hidden'
                     unoptimized
                     style={{
                         width: '40vw',
@@ -65,6 +65,19 @@ const Footer = () => {
             <div className='text-center xl:text-[1.1vw] text-[12px] text-[#888888] xl:mt-[17.6vh] mt-[72px]'>
                 Metaoptics Technologies Pte Ltd. All rights reserved
             </div>
+
+            <Image
+                src='/footer.png'
+                width='0'
+                height='0'
+                priority
+                className='h-auto w-[70vw] sm:w-[50vw] xl:relative absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[100%] xl:hidden block'
+                unoptimized
+                style={{
+                    objectFit: 'contain',
+                }}
+                alt='Footer Element'
+            />
         </div>
     );
 };
