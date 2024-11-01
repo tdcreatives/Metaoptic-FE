cc38255f961b796f89b65b5badbb850e829f2a96
TamNguyen <nguyenkhanhtam83@gmail.com>
2024-11-01 15:59:14 +0700

    contact-form

--- src/app/layouts/contact-us/form/index.js
+++ src/app/layouts/contact-us/form/index.js
@@ -4,31 +4,62 @@ const ContactUsForm = () => {
     return (
         <>
             <div className='xl:w-[40vw] mx-auto w-[90vw] xl:pb-[118px] pb-[48px]'>
-                <form className='xl:space-y-[32px] space-y-4'>
+                <form className='xl:space-y-[32px] space-y-4' method='POST' action='https://api.web3forms.com/submit'>
+                    <input
+                        type='hidden'
+                        name='access_key'
+                        value={process.env.WEB3FORM_ACCESS_KEY}
+                    />
+                    <input
+                        type='hidden'
+                        name='subject'
+                        value='New Submission from Metaoptics'
+                    />
+                    <input type='hidden'
+                        name='email'
+                        value={process.env.WEB3FORM_EMAIL}
+                    />
+                    <input
+                        type='hidden'
+                        name='redirect'
+                        value='https://web3forms.com/success'
+                    />
+                    <input
+                        type='checkbox'
+                        name='botcheck'
+                        id=''
+                        style={{ display: 'none' }}
+                    />
+                    <input
+                        type='hidden'
+                        name='from_name'
+                        value='Metaoptics Contact Us'
+                    />
                     <div className='flex space-x-4'>
                         <input
-                            type='text'
+                            type='text' name="customer_first_name"
                             placeholder='First Name'
                             className='futura-book xl:text-[24px] text-[16px] text-[#121212] placeholder-[#A7A7A7] bg-[#F5F5F5] w-full p-4 rounded-sm'
                         />
                         <input
-                            type='text'
+                            type='text' name="customer_last_name"
                             placeholder='Last Name'
                             className='futura-book xl:text-[24px] text-[16px] text-[#121212] placeholder-[#A7A7A7] bg-[#F5F5F5] w-full p-4 rounded-sm'
                         />
                     </div>
                     <input
-                        type='email'
+                        type='email' name="customer_email"
                         placeholder='Email'
                         className='futura-book xl:text-[24px] text-[16px] text-[#121212] placeholder-[#A7A7A7] bg-[#F5F5F5] w-full p-4 rounded-sm'
                     />
                     <input
-                        type='text'
+                        type='text' name="customer_subject"
                         placeholder='Subject'
                         className='futura-book xl:text-[24px] text-[16px] text-[#121212] placeholder-[#A7A7A7] bg-[#F5F5F5] w-full p-4 rounded-sm'
                     />
                     <textarea
                         placeholder='Message'
+                        name="customer_message"
                         className='futura-book xl:text-[24px] text-[16px] text-[#121212] placeholder-[#A7A7A7] bg-[#F5F5F5] w-full p-4 h-32 rounded-sm'></textarea>
                     <div className='flex justify-center'>
                         <button
