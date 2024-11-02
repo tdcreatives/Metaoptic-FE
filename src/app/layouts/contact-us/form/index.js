f0517be4192ea12bb315025bb75c293c3ae3c8d9
kt <khoa.ta@skillspar.com>
2024-11-02 12:54:41 +0700

    api

--- src/app/layouts/contact-us/form/index.js
+++ src/app/layouts/contact-us/form/index.js
@@ -1,76 +1,152 @@
-// import Image from 'next/image';
+'use client';
+
+import React, { useState } from 'react';
 
 const ContactUsForm = () => {
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        subject: '',
+        message: '',
+    });
+    const [status, setStatus] = useState({
+        message: '',
+        isSuccess: false,
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setStatus({ message: '', isSuccess: false });
+        setFormData((prevData) => ({ ...prevData, [name]: value }));
+    };
+
+    const validateForm = () => {
+        const { firstName, lastName, email, subject, message } = formData;
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!firstName || !lastName || !email || !subject || !message) {
+            setStatus({ message: 'All fields are required.', isSuccess: false });
+            return false;
+        }
+
+        if (!emailRegex.test(email)) {
+            setStatus({ message: 'Invalid email format.', isSuccess: false });
+            return false;
+        }
+
+        return true;
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!validateForm()) return;
+
+        const data = {
+            access_key: process.env.NEXT_PUBLIC_WEB3FORMS_ACCESS_TOKEN,
+            email: formData.email,
+            subject: formData.subject || process.env.NEXT_PUBLIC_SUBJECT,
+            message: formData.message,
+            replyto: process.env.NEXT_PUBLIC_REPLY_TO,
+            firstname: formData.firstName,
+            lastname: formData.lastName,
+            redirect: process.env.NEXT_PUBLIC_REDIRECT_URL,
+        };
+
+        try {
+            const response = await fetch('https://api.web3forms.com/submit', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+
+            if (response.ok) {
+                setStatus({ message: 'Form submitted successfully!', isSuccess: true });
+                setFormData({
+                    firstName: '',
+                    lastName: '',
+                    email: '',
+                    subject: '',
+                    message: '',
+                });
+            } else {
+                setStatus({
+                    message: 'Failed to submit the form. Please try again.',
+                    isSuccess: false,
+                });
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            setStatus({
+                message: 'An error occurred. Please try again.',
+                isSuccess: false,
+            });
+        }
+    };
+
     return (
-        <>
-            <div className='xl:w-[40vw] mx-auto w-[90vw] xl:pb-[118px] pb-[48px]'>
-                <form className='xl:space-y-[32px] space-y-4'>
-                    <div className='flex space-x-4'>
-                        <input
-                            type='text'
-                            placeholder='First Name'
-                            className='futura-book xl:text-[24px] text-[16px] text-[#121212] placeholder-[#A7A7A7] bg-[#F5F5F5] w-full p-4 rounded-sm'
-                        />
-                        <input
-                            type='text'
-                            placeholder='Last Name'
-                            className='futura-book xl:text-[24px] text-[16px] text-[#121212] placeholder-[#A7A7A7] bg-[#F5F5F5] w-full p-4 rounded-sm'
-                        />
-                    </div>
+        <div className='xl:w-[40vw] mx-auto w-[90vw] xl:pb-[118px] pb-[48px]'>
+            <form onSubmit={handleSubmit} className='xl:space-y-[32px] space-y-4'>
+                <div className='flex space-x-4'>
                     <input
-                        type='email'
-                        placeholder='Email'
+                        type='text'
+                        name='firstName'
+                        value={formData.firstName}
+                        onChange={handleChange}
+                        placeholder='First Name'
                         className='futura-book xl:text-[24px] text-[16px] text-[#121212] placeholder-[#A7A7A7] bg-[#F5F5F5] w-full p-4 rounded-sm'
                     />
                     <input
                         type='text'
-                        placeholder='Subject'
+                        name='lastName'
+                        value={formData.lastName}
+                        onChange={handleChange}
+                        placeholder='Last Name'
                         className='futura-book xl:text-[24px] text-[16px] text-[#121212] placeholder-[#A7A7A7] bg-[#F5F5F5] w-full p-4 rounded-sm'
                     />
-                    <textarea
-                        placeholder='Message'
-                        className='futura-book xl:text-[24px] text-[16px] text-[#121212] placeholder-[#A7A7A7] bg-[#F5F5F5] w-full p-4 h-32 rounded-sm'></textarea>
-                    <div className='flex justify-center'>
-                        <button
-                            type='submit'
-                            className='futura-book bg-[#D95442] text-white xl:text-[24px] text-[18px] py-2 xl:px-8 px-6 rounded-sm'>
-                            SUBMIT
-                        </button>
-                    </div>
-                </form>
-            </div>
-
-            {/* <div className='flex justify-center gap-[48px] xl:py-[118px] py-[48px]'>
-                {' '}
-                <Image
-                    width='0'
-                    height='0'
-                    priority
-                    unoptimized
-                    className='xl:w-[36px] xl:h-[36px] w-6 h-6 cursor-pointer'
-                    alt='Facebook'
-                    src='/facebook.svg'
+                </div>
+                <input
+                    type='email'
+                    name='email'
+                    value={formData.email}
+                    onChange={handleChange}
+                    placeholder='Email'
+                    className='futura-book xl:text-[24px] text-[16px] text-[#121212] placeholder-[#A7A7A7] bg-[#F5F5F5] w-full p-4 rounded-sm'
                 />
-                <Image
-                    width='0'
-                    height='0'
-                    priority
-                    unoptimized
-                    className='xl:w-[36px] xl:h-[36px] w-6 h-6 cursor-pointer'
-                    alt='Twitter'
-                    src='/twitter.svg'
+                <input
+                    type='text'
+                    name='subject'
+                    value={formData.subject}
+                    onChange={handleChange}
+                    placeholder='Subject'
+                    className='futura-book xl:text-[24px] text-[16px] text-[#121212] placeholder-[#A7A7A7] bg-[#F5F5F5] w-full p-4 rounded-sm'
                 />
-                <Image
-                    width='0'
-                    height='0'
-                    priority
-                    unoptimized
-                    className='xl:w-[36px] xl:h-[36px] w-6 h-6 cursor-pointer'
-                    alt='Instagram'
-                    src='/instagram.svg'
-                />
-            </div> */}
-        </>
+                <textarea
+                    name='message'
+                    value={formData.message}
+                    onChange={handleChange}
+                    placeholder='Message'
+                    className='futura-book xl:text-[24px] text-[16px] text-[#121212] placeholder-[#A7A7A7] bg-[#F5F5F5] w-full p-4 h-32 rounded-sm'></textarea>
+                <div className='flex justify-center'>
+                    <button
+                        type='submit'
+                        className='futura-book bg-[#D95442] text-white xl:text-[24px] text-[18px] py-2 xl:px-8 px-6 rounded-sm'>
+                        SUBMIT
+                    </button>
+                </div>
+                {status.message && (
+                    <p
+                        className={`text-center mt-4 ${
+                            status.isSuccess ? 'text-green-600' : 'text-red-600'
+                        }`}>
+                        {status.message}
+                    </p>
+                )}
+            </form>
+        </div>
     );
 };
 
