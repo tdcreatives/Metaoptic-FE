dda5416714efb6e081a2b321209993a26d0e77e0
macs <macs@gmail.com>
2024-12-30 11:52:29 +0700

    banner

--- src/app/layouts/homepage/banner/index.js
+++ src/app/layouts/homepage/banner/index.js
@@ -1,39 +1,38 @@
 'use client';
 
+import React from 'react';
 import Image from 'next/image';
 
-import { useRouter } from 'next/navigation';
-
 const Banner = () => {
-    const router = useRouter();
-    const handleOnNavigate = () => {
-        router.push('/contact-us');
-    };
     return (
-        <div className='max-w-[78vw] mx-auto relative mb-[5vh] flex items-center justify-center xl:h-[80vh] h-[300px]'>
-            <Image
-                src='/background-banner.svg'
-                alt='Background Banner'
-                width='0'
-                height='0'
-                className='w-full h-full cursor-pointer absolute top-0 left-0'
-                priority
+        <div className='relative w-full h-[calc(100vh-100px)] overflow-hidden'>
+            {/* Video Element */}
+            <video
+                className='absolute inset-0 w-full h-[70%] object-cover'
+                src='/animation.mp4'
+                autoPlay
+                muted
+                loop
+                playsInline
             />
 
-            <div className='relative z-10 flex flex-col pt-[3vh]'>
-                <p className='font-medium text-[#d44c39] tracking-widest uppercase text-center futura-book xl:text-[6vw] xl:leading-[6vw] leading-[40px] text-[32px]'>
-                    Metaoptics
-                </p>
+            {/* "View More" Button */}
+            <div className='absolute bottom-10 left-1/2 transform -translate-x-1/2 flex flex-col justify-center items-center'>
+                <Image
+                    src='/view-more.svg'
+                    alt='View more'
+                    width='0'
+                    height='0'
+                    className='w-[90px] h-auto cursor-pointer'
+                />
 
-                <p className='text-center futura-book xl:text-[1.8vw] text-[16px]'>
-                    Redefining what&lsquo;s possible in optical applications.
-                </p>
-
-                <button
-                    className='xl:px-[2vw] px-[24px] py-3 bg-[#d44c39] text-white futura-book w-fit mx-auto uppercase xl:text-[1.25vw] text-[12px] xl:mt-[5vh] mt-[24px] rounded-br-[32px]'
-                    onClick={handleOnNavigate}>
-                    Connect With Us
-                </button>
+                <div
+                    className='uppercase text-[20px] mt-[-16px] text-[#d34c39] ml-2'
+                    style={{
+                        letterSpacing: '6px',
+                    }}>
+                    View more
+                </div>
             </div>
         </div>
     );
