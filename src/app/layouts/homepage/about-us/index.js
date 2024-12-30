7674c53f0c7c38d05b87ff73fe8e1968a42def27
macs <macs@gmail.com>
2024-12-30 12:52:00 +0700

    about us

---
+++ src/app/layouts/homepage/about-us/index.js
@@ -0,0 +1,82 @@
+'use client';
+
+import React from 'react';
+import Image from 'next/image';
+
+const AboutUs = () => {
+    return (
+        <section className='relative flex h-[140vh] snap-start pt-16' id='about-us'>
+            {/* Left Content */}
+            <div className='flex flex-col'>
+                <h2 className='text-[150px] text-[#d34c39] uppercase futura-condensed-medium ml-16'>
+                    About Us
+                </h2>
+
+                {/* Content with Background */}
+                <div className='bg-gray-200 p-16 rounded-lg'>
+                    <p className='text-[20px] mb-6 max-w-[39%]'>
+                        At Metaoptics Technologies, we are breaking through current lens
+                        limitations in consumer and IoT applications.
+                    </p>
+
+                    <p className='text-[20px] leading-relaxed mb-6 max-w-[39%]'>
+                        Our high-throughput meta lens production ensures efficiency and
+                        cost-effectiveness. <br />
+                        As AR/VR and HUD technologies rapidly expands, our innovative meta
+                        lens are essential to provideminiaturization in supporting this
+                        market growth.
+                    </p>
+                    <button className='bg-[#d34c39] text-white font-bold text-lg ml-10 px-10 py-3 rounded-full futura-medium tracking-[4px] mt-6 text-[20px]'>
+                        FIND OUT MORE
+                    </button>
+                </div>
+            </div>
+
+            {/* Right Section with Overlapping Images */}
+            <div className='absolute right-0 top- w-[58%] h-full'>
+                <div className='grid grid-cols-2 gap-5'>
+                    <div className='flex flex-col gap-5'>
+                        <Image
+                            src='/about-1.svg'
+                            alt='About 1'
+                            width={0}
+                            height={0}
+                            sizes='100vw'
+                            className='rounded-lg shadow-lg w-full h-full rounded-[30px]'
+                        />
+
+                        <Image
+                            src='/about-3.png'
+                            alt='About 3'
+                            width={0}
+                            height={0}
+                            sizes='100vw'
+                            className='rounded-lg shadow-lg w-full h-full rounded-[30px]'
+                        />
+                    </div>
+
+                    <div className='flex flex-col gap-5 mb-[-40px] mt-10'>
+                        <Image
+                            src='/about-2.png'
+                            alt='About 2'
+                            width={0}
+                            height={0}
+                            sizes='100vw'
+                            className='rounded-lg shadow-lg w-full h-full rounded-[30px]'
+                        />
+                        <Image
+                            src='/about-4.png'
+                            alt='About 4'
+                            width={0}
+                            height={0}
+                            sizes='100vw'
+                            className='rounded-lg shadow-lg w-full h-full rounded-[30px]'
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default AboutUs;
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,16 @@
 import Header from './layouts/main/header';
 import Banner from './layouts/homepage/banner';
+import AboutUs from './layouts/homepage/about-us';
 import WhatWeDo from './layouts/homepage/what-we-do';
 import Products from './layouts/homepage/products';
 import Footer from './layouts/main/footer';
 
 export const metadata = {
     title: 'Metaoptics Technologies | Leading Metalens Manufacturer Singapore',
-    description: 'Metaoptics Technologies offers advanced metalens manufacturing for AR, VR, and IoT devices, delivering miniaturization and high performance to support next-gen optical tech',
-    keywords: 'Lens manufacturers, mobile phone cameras, camera lens manufacture, metalens manufacturer'
-
+    description:
+        'Metaoptics Technologies offers advanced metalens manufacturing for AR, VR, and IoT devices, delivering miniaturization and high performance to support next-gen optical tech',
+    keywords:
+        'Lens manufacturers, mobile phone cameras, camera lens manufacture, metalens manufacturer',
 };
 
 const Home = () => {
@@ -16,6 +18,7 @@ const Home = () => {
         <>
             <Header />
             <Banner />
+            <AboutUs />
             <WhatWeDo />
             <Products />
             <Footer />
