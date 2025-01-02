662faf27216157cd52369c40a5ee9cfcd2c4d5c3
kt <tndkhoaa0903@gmail.com>
2025-01-02 20:52:46 +0700

    build

---
+++ src/app/pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>{/* Add custom fonts or meta tags here */}</Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
+}
--- src/app/utils/index.js
+++ src/app/utils/index.js
@@ -1,5 +1,8 @@
 const isMobile = () => {
-    return window.innerWidth < 768;
+    if (typeof window !== 'undefined') {
+        return window.innerWidth < 768;
+    }
+    return false; // Default to false if `window` is not defined
 };
 
 export { isMobile };
