aa6dd95d28abb1a4221ef82923c8174302b33a8d
macs <macs>
2025-01-05 18:36:42 +0700

    build

---
+++ src/app/product/[slug]/ProductDetailsClientSide.js
@@ -0,0 +1,51 @@
+'use client';
+
+import React from 'react';
+
+import { useParams } from 'next/navigation';
+import data from '@/constants/data.json';
+
+import Header from '@/layouts/main/header';
+import Footer from '@/layouts/main/footer';
+
+import ProductDetailsBanner from '@/layouts/product-details/banner';
+import ProductDetailsSpecifications from '@/layouts/product-details/specifications';
+import RelatedProducts from '@/layouts/product-details/related-products';
+
+const ProductDetailsClientSide = () => {
+    const { slug } = useParams();
+
+    const product = data.products.find((item) => item.slug === slug);
+
+    if (!product) {
+        return (
+            <div className='flex flex-col items-center justify-center min-h-screen'>
+                <h1 className='text-4xl font-semibold text-red-500'>Product Not Found</h1>
+                <button
+                    onClick={() => router.push('/')}
+                    className='mt-4 bg-[#d34c39] text-white px-4 py-2 rounded-lg'>
+                    Go Back to Home
+                </button>
+                x
+            </div>
+        );
+    }
+
+    return (
+        <>
+            <Header background='#f0f0f0' />
+
+            <ProductDetailsBanner product={product} />
+
+            <ProductDetailsSpecifications
+                specifications={product?.details?.specifications}
+            />
+
+            <RelatedProducts relatedProducts={product?.details?.relatedProducts || []} />
+
+            <Footer />
+        </>
+    );
+};
+
+export default ProductDetailsClientSide;
--- src/app/product/[slug]/page.js
+++ src/app/product/[slug]/page.js
@@ -1,51 +1,26 @@
-'use client';
-
-import React from 'react';
-
-import { useParams } from 'next/navigation';
 import data from '@/constants/data.json';
 
-import Header from '@/layouts/main/header';
-import Footer from '@/layouts/main/footer';
+import ProductDetailsClientSide from './ProductDetailsClientSide';
 
-import ProductDetailsBanner from '@/layouts/product-details/banner';
-import ProductDetailsSpecifications from '@/layouts/product-details/specifications';
-import RelatedProducts from '@/layouts/product-details/related-products';
+export const generateStaticParams = async () => {
+    return data.products.map((product) => ({
+        slug: product.slug,
+    }));
+};
 
-const ProductDetails = () => {
-    const { slug } = useParams();
-
-    const product = data.products.find((item) => item.slug === slug);
+const ProductDetailsPage = async props => {
+    const params = await props.params;
+    const product = data.products.find((item) => item.slug === params.slug);
 
     if (!product) {
         return (
             <div className='flex flex-col items-center justify-center min-h-screen'>
                 <h1 className='text-4xl font-semibold text-red-500'>Product Not Found</h1>
-                <button
-                    onClick={() => router.push('/')}
-                    className='mt-4 bg-[#d34c39] text-white px-4 py-2 rounded-lg'>
-                    Go Back to Home
-                </button>
-                x
             </div>
         );
     }
 
-    return (
-        <>
-            <Header background='#f0f0f0' />
-
-            <ProductDetailsBanner product={product} />
-
-            <ProductDetailsSpecifications
-                specifications={product?.details?.specifications}
-            />
-
-            <RelatedProducts relatedProducts={product?.details?.relatedProducts || []} />
-
-            <Footer />
-        </>
-    );
+    return <ProductDetailsClientSide />;
 };
 
-export default ProductDetails;
+export default ProductDetailsPage;
