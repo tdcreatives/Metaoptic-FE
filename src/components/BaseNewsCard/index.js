a16ab1a20b86de59c2176b86a1a46338202160ac
kt <tndkhoaa0903@gmail.com>
2025-01-12 15:03:36 +0700

    redirect

--- src/components/BaseNewsCard/index.js
+++ src/components/BaseNewsCard/index.js
@@ -1,21 +1,32 @@
 'use client';
 
 import React from 'react';
+import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 
 const BaseNewsCard = ({ news }) => {
+    const router = useRouter();
+
+    const handleClick = () => {
+        console.log(news.external);
+        if (news.external) {
+            window.open(news.path, '_blank');
+        } else {
+            router.push(`/news/${news.slug}`);
+        }
+    };
     return (
-        <div className='flex flex-col gap-5 cursor-pointer hover:transform hover:scale-105 transition-transform transition-duration-300'>
-            <a href={`${news.path}`} target={news.external ? '_blank' : '_self'}>
-                <Image
-                    width={0}
-                    height={0}
-                    sizes='100vw'
-                    className='w-full cursor-pointer h-[250px] rounded-[20px]'
-                    src={`/${news?.image}`}
-                    alt='News'
-                />
-            </a>
+        <div
+            className='flex flex-col gap-5 cursor-pointer hover:transform hover:scale-105 transition-transform transition-duration-300'
+            onClick={handleClick}>
+            <Image
+                width={0}
+                height={0}
+                sizes='100vw'
+                className='w-full cursor-pointer h-[250px] rounded-[20px]'
+                src={`/${news?.image}`}
+                alt='News'
+            />
 
             <div className='flex flex-col gap-3'>
                 <div className='text-[12px] text-[#d34c39] uppercase'>{news.date}</div>
--- src/layouts/homepage/news/index.js
+++ src/layouts/homepage/news/index.js
@@ -6,34 +6,7 @@ import BaseButton from '@/components/BaseButton';
 
 import { useRouter } from 'next/navigation';
 
-const newsItems = [
-    {
-        title: 'MetaOptics coming to CES 2025 in Las Vegas',
-        desc: 'MetaOptics invites attendees to visit Booth 61015-1 to experience these cutting-edge products firsthand and explore its broader portfolio of metalens-powered solutions',
-        date: 'January 15, 2025',
-        image: 'news-1.jpg',
-        path: 'https://drive.google.com/file/d/1hNlMexABvVbvIh8IM3xZcrR4Qvv4jfqS/view',
-        external: true,
-    },
-    // {
-    //     title: 'Pico Projector using MEMS laser scanner',
-    //     desc: 'MetaOptics invites attendees to visit Booth 61015-1 to experience these cutting-edge products firsthand and explore its broader portfolio of metalens-powered solutions',
-    //     date: 'January 15, 2025',
-    //     image: 'news-2.jpg',
-    // },
-    // {
-    //     title: 'All about meta-tech you need to know',
-    //     desc: 'MetaOptics invites attendees to visit Booth 61015-1 to experience these cutting-edge products firsthand and explore its broader portfolio of metalens-powered solutions',
-    //     date: 'January 15, 2025',
-    //     image: 'news-3.jpg',
-    // },
-    // {
-    //     title: 'MetaOptics coming to CES 2025 in Las Vegas',
-    //     desc: 'MetaOptics invites attendees to visit Booth 61015-1 to experience these cutting-edge products firsthand and explore its broader portfolio of metalens-powered solutions',
-    //     date: 'January 15, 2025',
-    //     image: 'news-4.jpg',
-    // },
-];
+import data from '@/constants/news.json';
 
 const News = () => {
     const router = useRouter();
@@ -45,7 +18,7 @@ const News = () => {
             </h2>
 
             <div className='xl:p-16 p-8 grid xl:grid-cols-4 grid-cols-1 gap-4'>
-                {newsItems.map((news, index) => (
+                {data.news.map((news, index) => (
                     <BaseNewsCard key={index} news={news} />
                 ))}
             </div>
