480f6e5b58666a2e009f5ef3455f91066e36083d
kt <tndkhoaa0903@gmail.com>
2025-01-14 19:26:13 +0700

    footer

--- src/components/BaseProductCard/index.js
+++ src/components/BaseProductCard/index.js
@@ -85,7 +85,7 @@ const BaseProductCard = ({
                         className='absolute inset-0 z-10 flex justify-center items-center top-[-150px]'
                         initial={{ opacity: 0, scale: 0.95 }}
                         animate={{ opacity: 1, scale: 1 }}
-                        exit={{ opacity: 0, scale: 0.95 }}
+                        exit={{ opacity: 0, scale: 0.3 }}
                         transition={{ duration: 0.3, ease: 'easeInOut' }}>
                         <Image
                             src={`/${product?.image || 'fallback-image.svg'}`}
--- src/layouts/main/footer/index.js
+++ src/layouts/main/footer/index.js
@@ -8,15 +8,15 @@ const Footer = () => {
         <div className='bg-[#131313] futura-book px-[5.2vw] pt-4 pb-8 relative'>
             <div className='flex justify-between'>
                 <div className='flex flex-col'>
-                    <p className='text-[#888888] tracking-wider xl:text-[150px] text-[48px] futura-condensed-medium uppercase'>
+                    <p className='text-[#888888] tracking-wider xl:text-[84px] text-[48px] futura-condensed-medium uppercase'>
                         Connect with us
                     </p>
 
-                    <p className='text-[#d44c39] futura-medium xl:text-[30px] text-[24px] xl:mt-[-16px] mt-0'>
+                    <p className='text-[#d44c39] futura-medium xl:text-[24px] text-[18px] xl:mt-[-16px] mt-0'>
                         Metaoptics Technologies Pte Ltd
                     </p>
 
-                    <div className='futura-book xl:text-[25px] text-[18px] xl:mt-16 mt-8'>
+                    <div className='futura-book xl:text-[20px] text-[18px] xl:mt-8 mt-4'>
                         <a href='mailto:sales@metaoptics.sg'>
                             <p className='text-[#E0E1E0] mt-2'>
                                 {' '}
@@ -32,10 +32,12 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className='flex xl:flex-row flex-col xl:gap-0 gap-10 justify-between xl:mt-[160px] mt-10 w-full'>
-                <div className='flex gap-5 xl:flex-row flex-col text-white xl:text-[25px] text-[18px] futura-medium'>
+            <div className='flex xl:flex-row flex-col xl:gap-0 gap-10 justify-between xl:mt-[40px] mt-5 w-full'>
+                <div className='flex gap-5 xl:flex-row flex-col text-white xl:text-[18px] text-[14px] futura-medium'>
                     <div>
-                        <a href='javascript:void()' onClick={() => window.scrollTo(0, 0)}>Back to Top</a>
+                        <a href='javascript:void()' onClick={() => window.scrollTo(0, 0)}>
+                            Back to Top
+                        </a>
                     </div>
                     <div>
                         <a href='/products'>Our Products</a>
@@ -78,9 +80,9 @@ const Footer = () => {
                 </div> */}
             </div>
 
-            <div className='w-full h-[2px] my-4 bg-white'></div>
+            <div className='w-full h-[2px] my-2 bg-white'></div>
 
-            <div className='text-center text-[#868686] xl:text-[20px] text-[14px] uppercase tracking-widest'>
+            <div className='text-center text-[#868686] xl:text-[16px] text-[14px] uppercase tracking-widest'>
                 Metaoptics Technologies Pte Ltd. All rights reserved
             </div>
         </div>
