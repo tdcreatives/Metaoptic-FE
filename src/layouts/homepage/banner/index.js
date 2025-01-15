7f49f5180ee75418873b7c88da6ba49f8f1147b8
kt <tndkhoaa0903@gmail.com>
2025-01-15 19:29:22 +0700

    mobile animation

--- src/layouts/homepage/banner/index.js
+++ src/layouts/homepage/banner/index.js
@@ -3,13 +3,15 @@
 import React from 'react';
 import Image from 'next/image';
 
+import { isMobile } from 'react-device-detect';
+
 const Banner = () => {
     return (
-        <div className='relative w-full h-[calc(100vh-100px)] overflow-hidden'>
+        <div className='relative w-full xl:h-[calc(100vh-100px)] h-[60vh] overflow-hidden'>
             {/* Video Element */}
             <video
-                className='absolute inset-0 w-full h-[70%] xl:object-cover object-contain'
-                src='/animation.mp4'
+                className='absolute inset-0 w-full xl:h-[70%] h-[40vh] xl:object-cover object-contain'
+                src={isMobile ? '/mobile-animation.mp4' : '/animation.mp4'}
                 autoPlay
                 muted
                 loop
@@ -17,7 +19,11 @@ const Banner = () => {
             />
 
             {/* "View More" Button */}
-            <div className='absolute bottom-[72px] left-1/2 transform -translate-x-1/2 flex flex-col justify-center items-center' onClick={() => window.scrollTo({ top: window.innerHeight, behavior: 'smooth' })}>
+            <div
+                className='absolute bottom-[72px] left-1/2 transform -translate-x-1/2 flex flex-col justify-center items-center'
+                onClick={() =>
+                    window.scrollTo({ top: window.innerHeight, behavior: 'smooth' })
+                }>
                 <Image
                     src='/view-more.svg'
                     alt='View more'
