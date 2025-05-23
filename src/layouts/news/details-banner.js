bed9ea0875289454bb69c25430b99f91e3e4693f
TamNguyen <nguyenkhanhtam83@gmail.com>
2025-05-23 09:22:13 +0700

    update blog

--- src/layouts/news/details-banner.js
+++ src/layouts/news/details-banner.js
@@ -1,6 +1,7 @@
 'use client';
 
 import BaseCarousel from '@/components/BaseCarousel';
+import Image from 'next/image';
 
 const NewsDetailsBanner = ({ news = {} }) => {
     return (
@@ -8,8 +9,17 @@ const NewsDetailsBanner = ({ news = {} }) => {
             <div className='xl:text-[20px] text-[#D44C39] text-[16px]'>{news?.date}</div>
 
             <div className='xl:text-[40px] text-[28px]'>{news?.title}</div>
-
-            <BaseCarousel images={news?.details?.images || []} />
+            {news?.details?.images && news?.details?.images.length > 0 && (
+                news?.details?.images.length > 1 ? (
+                    <BaseCarousel images={news?.details?.images || []} />
+                ) : (
+                    <div className='relative xl:w-[70%] mt-10 max-w-[1200px] mx-auto w-full'>
+                        <div className='w-full xl:h-[30vw] h-[300px] relative max-h-[500px]'>
+                            <Image src={`/${news?.details?.images[0]}`} alt={news?.title} fill className='h-auto max-w-lg mx-auto' /> 
+                        </div>
+                    </div>                    
+                )
+            )}
         </div>
     );
 };
--- src/layouts/news/details-extra-images.js
+++ src/layouts/news/details-extra-images.js
@@ -4,28 +4,33 @@ import Image from 'next/image';
 import { motion } from 'framer-motion';
 
 const NewsDetailsExtraImages = ({ news = {}, className = '' }) => {
-    const [firstImage, secondImage, thirdImage] = news.details.extraImages;
+    const [firstImage, secondImage, thirdImage] = news.details.extraImages || [];
 
     return (
         <div className={className}>
             <div className='grid xl:grid-cols-3 grid-cols-1 gap-4 max-w-[1200px] mx-auto xl:w-[70%] w-[100%]'>
                 {/* Left large image */}
-                <div className='xl:col-span-2 col-span-1 h-[40vw] max-h-[400px] relative rounded-lg overflow-hidden shadow-lg'>
-                    <motion.div
-                        className='w-full h-full cursor-pointer'
-                        whileHover={{ opacity: 0.9 }}
-                        transition={{ type: 'spring' }}>
-                        <Image
+
+                {firstImage && (
+                    <div className='xl:col-span-2 col-span-1 h-[40vw] max-h-[400px] relative rounded-lg overflow-hidden shadow-lg'>
+                        <motion.div
+                            className='w-full h-full cursor-pointer'
+                            whileHover={{ opacity: 0.9 }}
+                            transition={{ type: 'spring' }}>
+                            <Image
                             src={`/${firstImage}`}
                             alt='Main extra view'
                             fill
                             className='object-cover rounded-lg'
                         />
                     </motion.div>
-                </div>
+                    </div>
+                )}
 
                 {/* Right stacked images */}
+                
                 <div className='grid grid-rows-2 gap-4'>
+                {secondImage && (
                     <div className='xl:h-full h-[40vw] max-h-[600px] relative rounded-lg overflow-hidden shadow-md'>
                         <motion.div
                             className='w-full h-full cursor-pointer'
@@ -38,9 +43,12 @@ const NewsDetailsExtraImages = ({ news = {}, className = '' }) => {
                                 className='object-cover rounded-lg'
                             />
                         </motion.div>
-                    </div>
-                    <div className='xl:h-full h-[40vw] max-h-[600px] relative rounded-lg overflow-hidden shadow-md'>
-                        <motion.div
+                        </div>
+                )}
+
+                {thirdImage && (
+                        <div className='xl:h-full h-[40vw] max-h-[600px] relative rounded-lg overflow-hidden shadow-md'>
+                            <motion.div
                             className='w-full h-full cursor-pointer'
                             whileHover={{ opacity: 0.9 }}
                             transition={{ type: 'spring', stiffness: 200, damping: 20 }}>
@@ -51,7 +59,8 @@ const NewsDetailsExtraImages = ({ news = {}, className = '' }) => {
                                 className='object-cover rounded-lg'
                             />
                         </motion.div>
-                    </div>
+                        </div>
+                )}
                 </div>
             </div>
         </div>
