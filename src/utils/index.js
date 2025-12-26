651b612c3559fcf2bbfc1b029b5f6cad7f957d21
kt <khoa.ta@skillspar.com>
2025-12-26 15:44:44 +0700

    Update dependencies and refactor ProductDetails components for improved readability and functionality

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,32 +1,39 @@
 const isMobile = () => {
-    if (typeof window !== 'undefined') {
-        return window.innerWidth < 768;
-    }
-    return false; // Default to false if `window` is not defined
+  if (typeof window !== "undefined") {
+    return window.innerWidth < 768;
+  }
+  return false; // Default to false if `window` is not defined
 };
 
 export { isMobile };
 
-
 export const truncateString = (str, maxLength) => {
-    if (str.length <= maxLength) {
-        return str;
-      }
-    
-      let truncated = '';
-      const words = str.split(' ');
-    
-      for (const word of words) {
-        if ((truncated + word).length + 1 > maxLength - 3) { // +1 for space, -3 for "..."
-          break;
-        }
-        truncated += (truncated === '' ? '' : ' ') + word;
-      }
-    
-      // Handle cases where even the first word exceeds maxLength
-      if (truncated === '' && words[0].length > maxLength - 3) {
-        return words[0].substring(0, maxLength - 3) + '...';
-      }
-    
-      return truncated.trim() + '...';
-  };
+  if (str.length <= maxLength) {
+    return str;
+  }
+
+  let truncated = "";
+  const words = str.split(" ");
+
+  for (const word of words) {
+    if ((truncated + word).length + 1 > maxLength - 3) {
+      // +1 for space, -3 for "..."
+      break;
+    }
+    truncated += (truncated === "" ? "" : " ") + word;
+  }
+
+  // Handle cases where even the first word exceeds maxLength
+  if (truncated === "" && words[0].length > maxLength - 3) {
+    return words[0].substring(0, maxLength - 3) + "...";
+  }
+
+  return truncated.trim() + "...";
+};
+
+export const removeAllBrTags = (html = "") => {
+  if (isMobile()) {
+    return html.replace(/<br\s*\/?>/g, "");
+  }
+  return html;
+};
