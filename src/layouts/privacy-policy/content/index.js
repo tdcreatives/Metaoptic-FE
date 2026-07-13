8b5c225bcb840296bed077569d061d3569c0a304
TamNguyen <nguyenkhanhtam83@gmail.com>
2026-07-13 10:50:22 +0700

    update content

--- src/layouts/privacy-policy/content/index.js
+++ src/layouts/privacy-policy/content/index.js
@@ -93,7 +93,14 @@ const Content = () => {
         },
         {
             title: 'How to Report',
-            content: `If you have any questions, concerns or complaints about our privacy practices, please contact us at <a href="mailto:dpo@metaoptics.sg" class="text-[#D34C39] hover:underline">dpo@metaoptics.sg</a>.`,
+            // content: `If you have any questions, concerns or complaints about our privacy practices, please contact us at <a href="mailto:dpo@metaoptics.sg" class="text-[#D34C39] hover:underline">dpo@metaoptics.sg</a>.`,
+            content: `To ensure independence, reports can be directed to our dedicated channel:
+
+            • Email: <a href="mailto:ethics@metaoptics.sg" class="text-[#D34C39] hover:underline">ethics@metaoptics.sg</a>.
+
+            Please provide as much detail as possible (e.g., names, dates, nature of the incident) to facilitate an effective investigation.
+            All reports are reviewd by the Audit Committee or designated independent personnel.
+            `,
         }
     ];
 
