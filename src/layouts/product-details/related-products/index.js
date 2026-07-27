f2cda65cd798467b7678e90d08443d5efad28448
TamNguyen <nguyenkhanhtam83@gmail.com>
2026-07-27 15:13:42 +0700

    Update image scaling in related products layout from scale-65 to scale-50 for improved visual consistency.

--- src/layouts/product-details/related-products/index.js
+++ src/layouts/product-details/related-products/index.js
@@ -28,7 +28,7 @@ const RelatedProducts = ({ relatedProducts }) => {
                         image={product.image}
                         category={product.category}
                         slug={product.slug}
-                        imageClassName="scale-65"
+                        imageClassName="scale-50"
                     />
                 ))}
             </div>
